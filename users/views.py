9ddca6929dd9f2059112169204f3f797ef17c855
Your Name <lemayiancaleb18@gmail.com>
2025-10-22 23:24:52 +0300

    update on register

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
+# user/views.py
+
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny # <-- Import this
 from .serializers import RegisterSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
-
+    # Add this line to allow unauthenticated users (anyone) to register
+    permission_classes = [AllowAny]
